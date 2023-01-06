92beb4687d4c2937d3c6d71f6f6e1d01618da4b8
leonshiyan <leonshiyan@gmail.com>
2023-01-05 16:02:08 -0800

    add data

--- data/skill-data.js
+++ data/skill-data.js
@@ -1,9 +1,16 @@
-const todos = [
-  {text: 'Feed llama', done: true, _id: 125223},
-  {text: 'Sleep under the stars', done: false, _id: 127904},
-  {text: 'Buy milk', done: false, _id: 139608},
-]
+const skills = [
+  {name: 'JavaScript', level: 'Expert', _id: 125223},
+  {name: 'HTML', level: 'Expert', _id: 127904},
+  {name: 'CSS', level: 'Expert', _id: 139608},
+  {name: 'Python', level: 'Intermediate', _id: 141513},
+  {name: 'C++', level: 'Intermediate', _id: 156203},
+  {name: 'Java', level: 'Intermediate', _id: 162901},
+  {name: 'PHP', level: 'Beginner', _id: 175508},
+  {name: 'Ruby', level: 'Beginner', _id: 189063},
+  {name: 'C#', level: 'Beginner', _id: 199304}
+];
+
 
 export { 
-	todos
+	skills
 }
