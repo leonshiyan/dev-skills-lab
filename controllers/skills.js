e692bed5abc4c140169a515db971cdb043b8871a
leonshiyan <leonshiyan@gmail.com>
2023-01-08 13:49:14 -0800

    add show function

--- controllers/skills.js
+++ controllers/skills.js
@@ -16,7 +16,34 @@ function index(req, res) {
 function newSkill(req,res){
   res.render('skills/new')
 }
+function create(req,res){
+  console.log(req.body)
+  req.body.current = false
+  Skill.create(req.body)
+  .then(skill =>{
+    res.redirect('/skills')
+  })
+  .catch(error => { 
+    console.log(error)
+    res.redirect('/')
+  })
+}
+function show(req, res) {
+  Skill.findById(req.params.id)
+  .then(skill => {
+    res.render('todos/show', {
+      skill: skill
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/todos')
+  })
+}
+
 export {
 	index,
   newSkill as new,
+  create,
+  show
 }
--- routes/skills.js
+++ routes/skills.js
@@ -10,11 +10,11 @@ router.get('/', skillsCtrl.index)
 // GET /todos/new
 router.get('/new', skillsCtrl.new)
 
-// // GET /todos/:id
-// router.get('/:id', skillsCtrl.show)
+// GET /todos/:id
+router.get('/:id', skillsCtrl.show)
 
-// // POST /todos
-// router.post('/', skillsCtrl.create)
+// POST /todos
+router.post('/', skillsCtrl.create)
 
 export {
   router
